a66a70b5c4f85a1aa238ab561188ec46d967cba9
jam-berg <114401197+jam-berg@users.noreply.github.com>
2022-09-27 16:29:50 +0200

    Added File for triple temp

---
+++ temp_sensor/temp_sensor.ino
@@ -0,0 +1,88 @@
+/*************************************************** 
+  This is a library for the Adafruit PT100/P1000 RTD Sensor w/MAX31865
+
+  Designed specifically to work with the Adafruit RTD Sensor
+  ----> https://www.adafruit.com/products/3328
+
+  This sensor uses SPI to communicate, 4 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Adafruit_MAX31865.h>
+
+Adafruit_MAX31865 thermo_sensors[3] = {
+    Adafruit_MAX31865(10, 11, 12, 13),
+    Adafruit_MAX31865(7, 8, 9, 13),
+    Adafruit_MAX31865(4, 5, 6, 13)
+  };
+// Use software SPI: CS, DI, DO, CLK
+// thermo_sensors[0] = Adafruit_MAX31865(10, 11, 12, 13);
+// thermo_sensors[1] = Adafruit_MAX31865(7, 8, 9, 13);
+// thermo_sensors[2] = Adafruit_MAX31865(3, 4, 5, 13);
+// use hardware SPI, just pass in the CS pin
+//Adafruit_MAX31865 thermo1 = Adafruit_MAX31865(10);
+
+// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
+#define RREF      430.0
+// The 'nominal' 0-degrees-C resistance of the sensor
+// 100.0 for PT100, 1000.0 for PT1000
+#define RNOMINAL  100.0
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
+
+  for (int i = 0; i < 3; i++) {
+    thermo_sensors[i].begin(MAX31865_4WIRE);  // set to 2WIRE or 4WIRE as necessary
+  }
+}
+
+
+void loop() {
+  for (int i = 0; i < 3; i++) { 
+    uint16_t rtd = thermo_sensors[i].readRTD();
+    Serial.print("Sensor Number: "); Serial.println(i);
+
+    Serial.print("RTD value: "); Serial.println(rtd);
+    float ratio = rtd;
+    ratio /= 32768;
+    Serial.print("Ratio = "); Serial.println(ratio,8);
+    Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
+    Serial.print("Temperature = "); Serial.println(thermo_sensors[i].temperature(RNOMINAL, RREF));
+  
+    // Check and print any faults
+    uint8_t fault = thermo_sensors[i].readFault();
+    if (fault) {
+      Serial.print("Fault 0x"); Serial.println(fault, HEX);
+      if (fault & MAX31865_FAULT_HIGHTHRESH) {
+        Serial.println("RTD High Threshold"); 
+      }
+      if (fault & MAX31865_FAULT_LOWTHRESH) {
+        Serial.println("RTD Low Threshold"); 
+      }
+      if (fault & MAX31865_FAULT_REFINLOW) {
+        Serial.println("REFIN- > 0.85 x Bias"); 
+      }
+      if (fault & MAX31865_FAULT_REFINHIGH) {
+        Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+      }
+      if (fault & MAX31865_FAULT_RTDINLOW) {
+        Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+      }
+      if (fault & MAX31865_FAULT_OVUV) {
+        Serial.println("Under/Over voltage"); 
+      }
+      thermo_sensors[i].clearFault();
+    }
+    Serial.println();
+  }
+  Serial.println();
+  Serial.println();
+  delay(1000);
+}
