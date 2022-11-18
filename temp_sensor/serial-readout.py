99b551548e187cf767a29f23540f1aed3719142f
pbueche <pbueche@student.ethz.ch>
2022-11-18 14:09:00 +0100

    fixed y0

--- temp_sensor/serial-readout.py
+++ temp_sensor/serial-readout.py
@@ -5,7 +5,7 @@ import serial
 import time
 import matplotlib.pyplot as plt
 import numpy as np
-
+import math
 now = datetime.now()
 current_time = now.strftime("%d_%m_%y_%H_%M_%S")
 print("Current Time =", current_time)
@@ -15,12 +15,25 @@ ser = serial.Serial('COM3', 9600, timeout=1)
 time.sleep(2)
 
 filename = "data_file_" + current_time + ".txt"
-f = open(filename, "a")
+#f = open(filename, "a")
 
 # defs für Auswertung
 cyclezeit = 45
-anzahlcycles = 1
+anzahlcycles = 2
 basetemperature = 0
+material="pom"
+
+
+#dichte & wärmeleitzahl der Matterialien
+if material == "pmma":
+    d=1180
+    c=1470
+if material=="pom":
+    d=1410
+    c=1500
+if material=="nylon":
+    d=1084
+    c=1700
 
 a = []
 b = []
@@ -36,7 +49,7 @@ while time.time() < t_finish:
         s = ser.readline()
         line = s.decode('utf-8')
         print(line[:-2])
-        f.write(line)  # Appends output to file
+        #f.write(line)  # Appends output to file
 
         a.append(line[:-2])
 
@@ -48,7 +61,7 @@ while time.time() < t_finish:
 
 result = []
 cycles = []
-print(len(b))
+
 
 for i in b:
     for j in i:
@@ -76,19 +89,70 @@ for u in range(len(cycles)):
     ja = []
     nein = []
 
-for u in range(0, anzahlcycles):
+ergebnis=[]
+
+yachsed=[]
+xachsed=[]
+qergebnis=[]
+thetaergebnis=[]
+
+
+fig=plt.figure(figsize=(9.5,4.5))
+ax1=fig.add_subplot(121)
+ax2=fig.add_subplot(122)
+
+
+for u in range(1, anzahlcycles):
     X0 = (max(daten[liste[2 * u]])-min(daten[liste[2*u]]))/2
     Y0 = (max(daten[liste[2 * u + 1]])-min(daten[liste[2 * u + 1]]))/2
     y_distance_index = find_index_nearest_to_0(daten[liste[2 * u]])
     y_distance_to_0 = daten[liste[2 * u + 1]][y_distance_index]
-    print(X0)
-    print(Y0)
-    print(Y0 - y_distance_to_0)
+    miny0=min(daten[liste[2 * u + 1]])
+    distance_y_to_0=Y0-(y_distance_to_0-miny0)
 
-    plt.plot(daten[liste[2 * u]], daten[liste[2 * u + 1]], marker="x", linestyle=" ")
+    #print(X0)
+    #print(Y0)
+    #print(distance_y_to_0)
 
-# plt.plot(daten["A"],daten["B"], color = "k")
-# plt.plot(daten["C"],daten["D"], marker = "x", color = "b", linestyle = " ")
+    q=X0/Y0
+    theta=np.arcsin(distance_y_to_0/Y0)
+    T=cyclezeit
+    D=(np.pi*0.000001)/(T*theta*math.log(X0/Y0,math.e))
 
+    yachsed.append(Y0)
+    xachsed.append(X0)
+    qergebnis.append(q)
+    thetaergebnis.append(theta)
+
+    lambdaeinzel=D*c*d
+
+    ergebnis.append(lambdaeinzel)
+    ax1.plot(daten[liste[2 * u]], daten[liste[2 * u + 1]], marker=".", linestyle=" ", label=str(u))
+
+
+
+'''
+
+print(X0)
+print(Y0)
+print(qergebnis)
+print(thetaergebnis)
+
+'''
+f=0
+for i in ergebnis:
+    f+=i
+
+
+wärmeleitfähigkeit=f/len(ergebnis   )
+print(ergebnis)
+print(wärmeleitfähigkeit)
+
+
+ax2.plot(ergebnis)
+ax2.plot(wärmeleitfähigkeit)
+
+plt.title(material)
+plt.legend()
 plt.savefig("data_file_" + current_time + ".png")
 plt.show()
